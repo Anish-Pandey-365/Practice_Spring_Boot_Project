8a238212b8631121dad0cd5c727223088830440c
Anish-Pandey-365 <anishpandey365@gmail.com>
2023-11-03 10:04:44 +0530

    first and Second Entity should be deleted

--- src/main/java/com/innoventes/test/app/entity/FirstEntity.java
+++
@@ -1,5 +0,0 @@
-package com.innoventes.test.app.entity;
-
-public class FirstEntity {
-    /// for testing first
-}
--- src/main/java/com/innoventes/test/app/entity/SecondEntity.java
+++
@@ -1,6 +0,0 @@
-package com.innoventes.test.app.entity;
-
-public class SecondEntity {
-    //for testing second
-
-}
